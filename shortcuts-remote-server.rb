a296be73f90ed41203fafd7328b95cf7c8fa9f48
Pavel Karpovich <pavel.karpovich@outlook.com>
2023-09-05 22:24:38 +0200

    Brew formula update for shortcuts-remote-server version v1.1.2

--- shortcuts-remote-server.rb
+++ shortcuts-remote-server.rb
@@ -5,21 +5,21 @@
 class ShortcutsRemoteServer < Formula
   desc ""
   homepage "https://github.com/pkarpovich/shortcuts-remote-server"
-  version "1.1.1"
+  version "1.1.2"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/pkarpovich/shortcuts-remote-server/releases/download/v1.1.1/shortcuts-remote-server_1.1.1_darwin_arm64.tar.gz"
-      sha256 "e1a4bb8e9cd681b947544e46ddfa4fb65708c4acc312e7d19d2fd5176a7bf719"
+      url "https://github.com/pkarpovich/shortcuts-remote-server/releases/download/v1.1.2/shortcuts-remote-server_1.1.2_darwin_arm64.tar.gz"
+      sha256 "5323efe52c06b3421814fdbc27b9170a51c7958156d1f02c29ce87e96a816e11"
 
       def install
         bin.install "shortcuts-remote-server"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/pkarpovich/shortcuts-remote-server/releases/download/v1.1.1/shortcuts-remote-server_1.1.1_darwin_amd64.tar.gz"
-      sha256 "29acac969cdbec66242f3d11fb7434246a16488a7ff0d13ac15f18bf68597533"
+      url "https://github.com/pkarpovich/shortcuts-remote-server/releases/download/v1.1.2/shortcuts-remote-server_1.1.2_darwin_amd64.tar.gz"
+      sha256 "57e68352017f9dba492d75ace421b1dad2c5e18b7528a66885ab367efa74bc98"
 
       def install
         bin.install "shortcuts-remote-server"
